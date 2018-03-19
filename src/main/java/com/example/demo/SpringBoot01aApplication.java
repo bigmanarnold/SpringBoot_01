38e3aee5989dbc9ef9039d9b9c79aaa12ca1315e
bigmanarnold <arnoldchonai@gmail.com>
2018-03-19 16:33:42 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot01aApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot01aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot01aApplication.class, args);
+	}
+}
